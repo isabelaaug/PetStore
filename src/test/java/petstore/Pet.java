652a7207e0efeee6bed3ba0819e6c600bc8fca6c
isabelaaug <isabela.augusta@hotmail.com>
2022-02-01 00:24:07 -0300

    Finalizado script de excluir Pet

--- src/test/java/petstore/Pet.java
+++ src/test/java/petstore/Pet.java
@@ -79,4 +79,22 @@ public class Pet {
                 .body("name", is("Bob"))
                 .body("status", is("unavailable"));
     }
+
+    // DELETE
+    @Test(priority = 4)
+    public void excluirPet(){
+        String petId = "199678586521548500";
+
+        given()
+                .contentType("application/json")
+                .log().all()
+        .when()
+                .delete(uri + "/" + petId)
+        .then()
+                .log().all()
+                .statusCode(200)
+                .body("code", is(200))
+                .body("type", is("unknown"))
+                .body("message", is(petId));
+    }
 }
